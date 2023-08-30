3bd4ed364cfe89bfa1ffc613f3d786428046c0de
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-08-30 02:45:50 +0200

    actualizando estilos

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,87 +1,5 @@
+
 <!--<script>
-	import Header from './Header.svelte';
-	import './styles.css';
-</script>
-
-<div class="app">
-	<Header />
-
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<div class="card">
-			<h2>¡Organiza tus tareas diarias!</h2>
-			<p>Utiliza nuestra aplicación para llevar un seguimiento de tus tareas diarias y mantener tu vida organizada.</p>
-			<a href="#">Empieza ahora</a>
-		</div>
-	</footer>
-</div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	.card {
-		background-color: #f4f4f4;
-		padding: 20px;
-		border-radius: 10px;
-		box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-		text-align: center;
-	}
-
-	.card h2 {
-		margin: 0 0 10px;
-		font-size: 24px;
-	}
-
-	.card p {
-		margin: 0 0 20px;
-		color: #555;
-	}
-
-	.card a {
-		display: inline-block;
-		padding: 10px 20px;
-		background: linear-gradient(45deg, #ff00cc, #3333ff);
-		color: white;
-		border-radius: 5px;
-		text-decoration: none;
-		transition: background-color 0.3s ease;
-	}
-
-	
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>-->
-<script>
 	import Header from './Header.svelte';
 	import './styles.css';
 </script>
@@ -115,7 +33,7 @@
 				<h3>Objetivos</h3>
 				<p>Define objetivos específicos y trabaja para alcanzarlos.</p>
 			</div>
-			<!-- Agrega más categorías aquí -->
+		
 		</div>
 	</footer>
 </div>
@@ -182,6 +100,135 @@ main {
 
 
 
+</style>-->
+
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<div class="categories">
+			<div class="category">
+				<div class="category-header">
+					<span class="category-icon">📝</span>
+					<h3>Tareas Diarias</h3>
+				</div>
+				<p>Mantén un registro de tus tareas cotidianas.</p>
+			</div>
+			<div class="category">
+				<div class="category-header">
+					<span class="category-icon">📌</span>
+					<h3>Notas Rápidas</h3>
+				</div>
+				<p>Guarda notas rápidas y recordatorios.</p>
+			</div>
+			<div class="category">
+				<div class="category-header">
+					<span class="category-icon">⏰</span>
+					<h3>Recordatorios</h3>
+				</div>
+				<p>Programa recordatorios para no olvidar tus actividades.</p>
+			</div>
+			<div class="category">
+				<div class="category-header">
+					<span class="category-icon">🎯</span>
+					<h3>Metas</h3>
+				</div>
+				<p>Establece y sigue tus metas personales y profesionales.</p>
+			</div>
+			<div class="category">
+				<div class="category-header">
+					<span class="category-icon">🎉</span>
+					<h3>Objetivos</h3>
+				</div>
+				<p>Define objetivos específicos y trabaja para alcanzarlos.</p>
+			</div>
+			<!-- Agrega más categorías aquí -->
+		</div>
+	</footer>
+</div>
+
+<style>
+	/* Estilos previos */
+
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	.categories {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+		grid-gap: 20px;
+		justify-items: center;
+		margin-top: 40px;
+	}
+
+	.category {
+		background-color: #fff;
+		padding: 20px;
+		border-radius: 10px;
+		box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+		text-align: center;
+		transition: transform 0.3s ease, background-color 0.3s ease;
+		cursor: pointer;
+	}
+
+	.category:hover {
+		transform: translateY(-5px);
+		background-color: #f4f4f4;
+	}
+
+	.category h3 {
+		margin: 0 0 10px;
+		font-size: 20px;
+		color: var(--color-theme-1);
+	}
+
+	.category p {
+		margin: 0;
+		color: #555;
+	}
+
+	.category-header {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-bottom: 15px;
+	}
+
+	.category-icon {
+		font-size: 24px;
+		margin-right: 10px;
+	}
+
+	@media (max-width: 768px) {
+		.categories {
+			grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+		}
+	}
 </style>
+
+
 
 	
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,272 +1,4 @@
-
-
-<!--<script>
-	import Counter from './Counter.svelte';
-</script>
-
-<svelte:head>
-	<title>Inicio</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<span class="letter">B</span>
-			<span class="letter">I</span>
-			<span class="letter">E</span>
-			<span class="letter">N</span>
-			<span class="letter">V</span>
-			<span class="letter">E</span>
-			<span class="letter">N</span>
-			<span class="letter">I</span>
-			<span class="letter">D</span>
-			<span class="letter">A</span>
-		</span>
-		<br />
-		<span class="subtitle">A tu</span><br />
-		<span class="main-title">Plan de vida</span>
-	</h1>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-		background-color: #f7f7f7;
-	}
-
-	h1 {
-		width: 100%;
-		text-align: center;
-		margin: 0;
-		padding: 40px 0;
-		font-family: 'Helvetica Neue', sans-serif;
-	}
-
-	.welcome {
-		font-size: 48px;
-		display: block;
-		color: #ff00cc;
-		margin-bottom: 20px;
-	}
-
-	.letter {
-		display: inline-block;
-		transform: translateY(-100%);
-		animation: slideInTop 0.5s ease-in-out forwards;
-		opacity: 0;
-		color: #ff00cc;
-		font-size: 48px;
-	}
-
-	.subtitle {
-		font-size: 24px;
-		color: #666;
-		margin-bottom: 5px;
-		opacity: 0;
-		animation: fadeIn 1s ease-in-out forwards;
-	}
-
-	.main-title {
-		font-size: 36px;
-		color: #333;
-		opacity: 0;
-		animation: fadeIn 1s ease-in-out forwards;
-	}
-
-	@keyframes slideInTop {
-		to {
-			transform: translateY(0);
-			opacity: 1;
-		}
-	}
-
-	@keyframes fadeIn {
-		to {
-			opacity: 1;
-		}
-	}
-</style>-->
-
-<!--<script>
-	import Counter from './Counter.svelte';
-</script>
-
-<svelte:head>
-	<title>Inicio</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<span class="letter">B</span>
-			<span class="letter">I</span>
-			<span class="letter">E</span>
-			<span class="letter">N</span>
-			<span class="letter">V</span>
-			<span class="letter">E</span>
-			<span class="letter">N</span>
-			<span class="letter">I</span>
-			<span class="letter">D</span>
-			<span class="letter">A</span>
-		</span>
-		<br />
-		<span class="subtitle">A tu</span><br />
-		<span class="main-title">Plan de vida</span>
-	</h1>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-		background-color: #1e1e1e;
-	}
-
-	h1 {
-		width: 100%;
-		text-align: center;
-		margin: 0;
-		padding: 40px 0;
-		font-family: 'Helvetica Neue', sans-serif;
-	}
-
-	.welcome {
-		font-size: 48px;
-		display: block;
-		background-image: linear-gradient(45deg, #ff00cc, #3333ff);
-		-webkit-background-clip: text;
-		color: transparent;
-		margin-bottom: 20px;
-		animation: gradientFade 2s infinite alternate;
-	}
-
-	.letter {
-		display: inline-block;
-	}
-
-	.subtitle {
-		font-size: 24px;
-		color: #999;
-		margin-bottom: 5px;
-		opacity: 0.8;
-	}
-
-	.main-title {
-		font-size: 36px;
-		color: #fff;
-	}
-
-	@keyframes gradientFade {
-		from {
-			-webkit-background-clip: text;
-			color: transparent;
-		}
-		to {
-			-webkit-background-clip: unset;
-			color: inherit;
-		}
-	}
-</style>-->
-
-
-<!--<script>
-	import Counter from './Counter.svelte';
-</script>
-
-<svelte:head>
-	<title>Inicio</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<span class="letter">B</span>
-			<span class="letter">I</span>
-			<span class="letter">E</span>
-			<span class="letter">N</span>
-			<span class="letter">V</span>
-			<span class="letter">E</span>
-			<span class="letter">N</span>
-			<span class="letter">I</span>
-			<span class="letter">D</span>
-			<span class="letter">A</span>
-		</span>
-		<br />
-		<span class="subtitle">A tu</span><br />
-		<span class="main-title">Plan de vida</span>
-	</h1>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-		background-color: #000;
-	}
-
-	h1 {
-		width: 100%;
-		text-align: center;
-		margin: 0;
-		padding: 40px 0;
-		font-family: 'Helvetica Neue', sans-serif;
-	}
-
-	.welcome {
-		font-size: 48px;
-		display: block;
-		background-image: linear-gradient(45deg, #f06, #0cf);
-		-webkit-background-clip: text;
-		color: transparent;
-		animation: gradientFlow 2s infinite alternate;
-		text-shadow: 0 3px 6px rgba(0, 0, 0, 0.5);
-	}
-
-	.letter {
-		display: inline-block;
-	}
-
-	.subtitle {
-		font-size: 24px;
-		color: #888;
-		margin-bottom: 5px;
-	}
-
-	.main-title {
-		font-size: 36px;
-		color: #fff;
-	}
-
-	@keyframes gradientFlow {
-		from {
-			background-position: 100% 0;
-		}
-		to {
-			background-position: 0 100%;
-		}
-	}
-</style>-->
-<!--<script>
-	import Counter from './Counter.svelte';
+<script>
 </script>
 
 <svelte:head>
@@ -275,35 +7,76 @@
 </svelte:head>
 
 <section class="main-section">
-	<h1>
-		<span class="typing-title">¡Bienvenida a tu Plan de Vida!</span>
-		<br />
-		<span class="subtitle">Organiza y sigue tus metas diarias</span>
-	</h1>
+	<div class="gradient-header">
+		<h1>
+			<span class="typing-title">¡Bienvenida a tu Plan de Vida!</span>
+			<br />
+			<span class="subtitle">Organiza y sigue tus metas diarias</span>
+		</h1>
+	</div>
 
-	<Counter />
+	<div class="mood-section">
+		<h2>¿Cómo te sientes hoy?</h2>
+		<div class="mood-options">
+			<div class="mood-option">
+				<p>😄</p>
+				<span class="mood-label">Feliz</span>
+			</div>
+			<div class="mood-option">
+				<p>😢</p>
+				<span class="mood-label">Triste</span>
+			</div>
+			<div class="mood-option">
+				<p>😃</p>
+				<span class="mood-label">Emocionado</span>
+			</div>
+			<div class="mood-option">
+				<p>😫</p>
+				<span class="mood-label">Estresado</span>
+			</div>
+			<div class="mood-option">
+				<p>😎</p>
+				<span class="mood-label">Relajado</span>
+			</div>
+			<div class="mood-option">
+				<p>😊</p>
+				<span class="mood-label">Contento</span>
+			</div>
+			<div class="mood-option">
+				<p>😒</p>
+				<span class="mood-label">Aburrido</span>
+			</div>
+			<div class="mood-option">
+				<p>😇</p>
+				<span class="mood-label">Tranquilo</span>
+			</div>
+			<!-- Agrega más opciones de estado de ánimo -->
+		</div>
+	</div>
 </section>
 
 <style>
+	/* Estilos previos */
 	.main-section {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		flex: 0.1;
-		background: linear-gradient(45deg, #ff00cc, #3333ff);
-		height: 100vh;
+	}
 
+	.gradient-header {
+		background: linear-gradient(45deg, #ff00cc, #3333ff);
+		text-align: center;
+		padding: 30px 0;
+		height: 150px; 
+		width: 100%;
 	}
 
 	h1 {
-		width: 100%;
-		text-align: center;
 		margin: 0;
-		padding: 40px 0;
 		font-family: 'Helvetica Neue', sans-serif;
 		color: #fff;
-		background: linear-gradient(45deg, #ff00cc, #3333ff);
 		background-clip: text;
 		-webkit-background-clip: text;
 	}
@@ -317,7 +90,7 @@
 	}
 
 	.subtitle {
-		font-size: 24px;
+		font-size: 22px;
 		color: lightgray;
 		margin-top: 15px;
 	}
@@ -336,327 +109,16 @@
 			border-color: transparent;
 		}
 	}
-</style>-->
-
-
-<!--<script>
-	import Counter from './Counter.svelte';
-</script>
-
-<svelte:head>
-	<title>Inicio</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="typing-title">¡Bienvenido a tu Plan de Vida!</span>
-		<br />
-		<span class="subtitle typing-subtitle">Organiza y sigue tus metas diarias</span>
-	</h1>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-		background-color: #000;
-		height: 100vh;
-	}
-
-	h1 {
-		width: 100%;
-		text-align: center;
-		margin: 0;
-		padding: 40px 0;
-		font-family: 'Helvetica Neue', sans-serif;
-		color: #fff;
-	}
-
-	.typing-title,
-	.typing-subtitle {
-		display: inline-block;
-		overflow: hidden;
-		white-space: nowrap;
-		border-right: 2px solid #fff;
-		letter-spacing: 2px;
-		animation: typing 3s steps(40, end), blink-caret 0.75s step-end infinite;
-	}
-
-	.typing-subtitle {
-		animation: typing 3s steps(40, end), blink-caret 0.75s step-end infinite, delay 3s ease-out;
-	}
-
-	@keyframes typing {
-		from {
-			width: 0;
-		}
-		to {
-			width: 100%;
-		}
-	}
-
-	@keyframes blink-caret {
-		from,
-		to {
-			border-color: transparent;
-		}
-		50% {
-			border-color: #fff;
-		}
-	}
-
-	@keyframes delay {
-		from {
-			visibility: visible;
-		}
-		to {
-			visibility: hidden;
-		}
-	}
-</style>-->
-
-<!--<script>
-	import Counter from './Counter.svelte';
-</script>
-
-<svelte:head>
-	<title>Inicio</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="title">¡Bienvenido a tu</span>
-		<br />
-		<span class="title highlight">Plan de Vida!</span>
-	</h1>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-		background-color: #000;
-		height: 100vh;
-	}
-
-	h1 {
-		width: 100%;
-		text-align: center;
-		margin: 0;
-		padding: 40px 0;
-		font-family: 'Helvetica Neue', sans-serif;
-		color: #fff;
-	}
-
-	.title {
-		font-size: 36px;
-		animation: move 2s infinite, changeColor 4s infinite;
-	}
-
-	.title.highlight {
-		font-size: 48px;
-		animation: move 2s infinite reverse, changeColor 4s infinite reverse;
-	}
-
-	@keyframes move {
-		0%, 100% {
-			transform: translateY(0);
-		}
-		25% {
-			transform: translateY(-10px);
-		}
-		50% {
-			transform: translateY(0);
-		}
-		75% {
-			transform: translateY(10px);
-		}
-	}
-
-	@keyframes changeColor {
-		0% {
-			color: #ff00cc;
-		}
-		25% {
-			color: #3333ff;
-		}
-		50% {
-			color: #00ff00;
-		}
-		75% {
-			color: #ff0000;
-		}
-		100% {
-			color: #ff00cc;
-		}
-	}
-</style>-->
-
-<!--<script>
-	import Counter from './Counter.svelte';
-</script>
-
-<svelte:head>
-	<title>Inicio</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1 class="title">
-		<span class="word">Bienvenido</span>
-		<br />
-		<span class="word">a tu</span>
-		<br />
-		<span class="word">Plan de Vida</span>
-	</h1>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-		background-color: #f0f0f0;
-		height: 100vh;
-		overflow: hidden;
-	}
-
-	.title {
-		text-align: center;
-		margin: 0;
-		padding: 40px 0;
-		font-family: 'Helvetica Neue', sans-serif;
-		font-size: 24px;
-		color: #333;
-	}
-
-	.word {
-		display: block;
-		opacity: 0;
-		transform: scale(0.8) rotate(-45deg);
-		animation: rotateIn 1s ease-in-out, fadeInUp 1.5s ease-in-out;
-		animation-fill-mode: forwards;
-	}
-
-	.word:nth-child(2) {
-		animation-delay: 0.5s;
-	}
-
-	.word:nth-child(3) {
-		animation-delay: 1s;
-	}
-
-	@keyframes rotateIn {
-		0% {
-			opacity: 0;
-			transform: scale(0.8) rotate(-45deg);
-		}
-		100% {
-			opacity: 1;
-			transform: scale(1) rotate(0);
-		}
-	}
-
-	@keyframes fadeInUp {
-		0% {
-			opacity: 0;
-			transform: translateY(30px);
-		}
-		100% {
-			opacity: 1;
-			transform: translateY(0);
-		}
-	}
-</style>-->
-
-
-
-<script>
-	import Counter from './Counter.svelte';
-</script>
-
-<svelte:head>
-	<title>Inicio</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section class="main-section">
-	<h1>
-		<span class="typing-title">¡Bienvenida a tu Plan de Vida!</span>
-		<br />
-		<span class="subtitle">Organiza y sigue tus metas diarias</span>
-	</h1>
-
-	<Counter />
-
-	<div class="mood-section">
-		<h2>¿Cómo te sientes hoy?</h2>
-		<div class="mood-options">
-			<div class="mood-option">
-				<p>😄</p>
-				<span>Feliz</span>
-			</div>
-			<div class="mood-option">
-				<p>😢</p>
-				<span>Triste</span>
-			</div>
-			<div class="mood-option">
-				<p>😃</p>
-				<span>Emocionado</span>
-			</div>
-			<div class="mood-option">
-				<p>😫</p>
-				<span>Estresado</span>
-			</div>
-			<div class="mood-option">
-				<p>😎</p>
-				<span>Relajado</span>
-			</div>
-			<div class="mood-option">
-				<p>😊</p>
-				<span>Contento</span>
-			</div>
-			<div class="mood-option">
-				<p>😒</p>
-				<span>Aburrido</span>
-			</div>
-			<div class="mood-option">
-				<p>😇</p>
-				<span>Tranquilo</span>
-			</div>
-			<!-- Agrega más opciones de estado de ánimo -->
-		</div>
-	</div>
-</section>
-
-<style>
-	/* Estilos previos */
 
 	.mood-section {
-		margin-top: 40px;
+		margin-top: 20px;
 		text-align: center;
 	}
 
 	.mood-section h2 {
 		font-size: 24px;
 		color: #333;
-		margin-bottom: 20px;
+		margin-bottom: 17px;
 	}
 
 	.mood-options {
@@ -685,9 +147,9 @@
 		transition: opacity 0.2s ease-in-out;
 	}
 
-	.mood-option span {
+	.mood-label {
 		font-size: 12px;
-		color: #555;
+		color: #fff;
 		opacity: 0.8;
 	}
 
@@ -699,17 +161,3 @@
 		opacity: 1;
 	}
 </style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
