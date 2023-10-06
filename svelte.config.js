be133ad89ba6f4d9cad14c7f0ab48373a7081ecd
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-06 14:43:01 +0200

    intentando otra vez visualizar el proyecto en githubpages

--- svelte.config.js
+++ svelte.config.js
@@ -4,13 +4,11 @@ import adapter from '@sveltejs/adapter-auto';
 export default {
   kit: {
     adapter: adapter(),
-    target: '#svelte',
-    paths: {
-      base: '/LifePlanHub', // Ajusta esto según tu configuración de GitHub Pages
-    },
-    // ... otras configuraciones ...
+    // ... otras configuraciones si las tienes
   },
 };
+
+
 
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  // ... otras configuraciones si las tienes
 });
