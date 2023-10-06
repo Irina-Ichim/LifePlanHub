801bd6b17bb7205762a6d9ef642827d91c837ca5
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-06 23:04:49 +0200

    solucionando errores

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
 export default {
-  // ... Otras configuraciones
   kit: {
-    // ... Otras configuraciones de SvelteKit
     adapter: adapter(),
-  },
+    // Otras configuraciones de SvelteKit
+  }
 };
+
 
 
 
